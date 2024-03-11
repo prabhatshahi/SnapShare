2d00b569f837c26ddd5d57a996e2f82b3473684b
Prabhat Ranjan Shahi <ranjanprabhat555@gmail.com>
2024-03-11 12:29:32 +0530

    .

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,16 @@
-import { Button } from "@chakra-ui/react";
+import { Button, Container } from "@chakra-ui/react";
+import { Routes, Route } from "react-router-dom"; // Import Route component
+import Header from "./components/Header";
 
 function App() {
   return (
-    <>
-      <Button>Hello</Button>
-    </>
+    <Container maxW="620px">
+      <Header />
+      <Routes>
+        <Route path="/:username" element={<UserPage />} />
+        <Route path="/:username/post/:pid" element={<PostPage />} />
+      </Routes>
+    </Container>
   );
 }
 
--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,7 @@ import { extendTheme, ChakraProvider, ColorModeScript } from "@chakra-ui/react";
 import { mode } from "@chakra-ui/theme-tools"; // Import mode function from Chakra UI
 import App from "./App.jsx";
 import "./index.css";
+import { BrowserRouter } from "react-router-dom";
 
 const styles = {
   global: (props) => ({
@@ -29,9 +30,11 @@ const theme = extendTheme({ config, styles, colors });
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <ChakraProvider theme={theme}>
-      <ColorModeScript initialColorMode={theme.config.initialColorMode} />
-      <App />
-    </ChakraProvider>
+    <BrowserRouter>
+      <ChakraProvider theme={theme}>
+        <ColorModeScript initialColorMode={theme.config.initialColorMode} />
+        <App />
+      </ChakraProvider>
+    </BrowserRouter>
   </React.StrictMode>
 );
